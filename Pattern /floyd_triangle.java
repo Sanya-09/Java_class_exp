de9a8ab9415abe00bc4e8fd46a48aba9423c4b6c
Sanya-09 <agarwalsanya90@gmail.com>
2025-02-01 18:17:53 +0530

    Create floyd_triangle.java

---
+++ Pattern /floyd_triangle.java
@@ -0,0 +1,23 @@
+public class floyd_triangle{
+public static void flyod_tri(int n){
+        int counter = 1;
+           // n = number of level in pyramid
+        // outer loops for number of lines 
+        for(int i=1 ; i<=n ;i++){
+            // inner loop 1 for spaces 
+            for (int j = 1 ; j <= i; j++){
+                System.out.print(counter);
+                counter++;
+            }
+            // inner loop 2 for stars
+            for (int j = 1 ; j<=i ; j++){
+                System.out.print(" ");
+            }
+            
+            System.out.println();
+        }
+    }
+public static void main(String[] args) {
+        flyod_tri(5);
+}
+}
